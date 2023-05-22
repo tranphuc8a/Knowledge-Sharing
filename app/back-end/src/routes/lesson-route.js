eecaeb96223dabc564306c52cb96a4e3e116c790
DESKTOP-OET0P0K\Phuc <tranphuc8a@gmail.com>
2023-05-22 15:36:35 +0700

    update mailservice

--- app/back-end/src/routes/lesson-route.js
+++ app/back-end/src/routes/lesson-route.js
@@ -1,5 +1,8 @@
 
-require AuthController = require('../controllers/auth-controller');
+var AuthController = require('../controllers/auth-controller');
+var mailer = require('../services/email-service');
+
+let content = `121341543`;
 
 class LessonRoute{
 	constructor(app){
@@ -9,9 +12,18 @@ class LessonRoute{
 
 	async route(){
 		this.app.get('/api/lesson/', this.authcontroller.login , function(req, res, next){
-			res.send("call api lesson");
+			mailer.send({
+				from: 'Knowledge-Sharing system',
+				to: 'nhungthisope123@gmail.com',
+				subject: 'Email thông báo mã lấy lại mật khẩu',
+				text: "<br> <br> <i>content </i>"
+			});
+			res.send("Send email success");
+
 		});
 	}
 }
 
+
+module.exports = LessonRoute;
 
