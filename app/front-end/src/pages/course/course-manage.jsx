d7bd684b0d263a5e49a2a67a11157e1bc0c32259
DESKTOP-OET0P0K\Phuc <tranphuc8a@gmail.com>
2023-06-28 23:40:16 +0700

    Complete course manage page

--- app/front-end/src/pages/course/course-manage.jsx
+++ app/front-end/src/pages/course/course-manage.jsx
@@ -16,6 +16,10 @@ import Banner from '../../components/layout/banner/banner';
 import PostAPI from '../../services/api/post-api';
 import Popup from '../../components/popup/popup';
 import ChooseLessonPopup from '../../components/course/choose-lesson-popup/choose-lesson-popup';
+import LessonCard from '../../components/lesson/lesson-card';
+import {BiTrash as Trash} from 'react-icons/bi';
+import DeleteAPI from '../../services/api/delete-api';
+import PatchAPI from '../../services/api/patch-api';
 
 
 
@@ -78,6 +82,10 @@ class CourseManage extends React.Component{
             return this.nullCourse("Không có quyền quản lý khóa học");
 
         let listLesson = this.state.listLesson;
+        listLesson.sort((lessonA, lessonB) => lessonA.offset - lessonB.offset);
+        listLesson.forEach((lesson, index) => {
+            lesson.offset = index;
+        })
 
         return <Layout header={<Header active={1}/>} >
             <Banner />
@@ -94,9 +102,43 @@ class CourseManage extends React.Component{
                     "Hiện chưa có bài học nào" :
                     this.getContent()
                 }
+                <Separate style={{margin: '12px 0px 24px 0px'}} />
+                <div>
+                    <Button text = "Lưu lại" onclick = {this.saveListLesson } />
+                    <Button text = "Trở lại khóa học" onclick = {this.backCourse } style = {{margin: '0px 12px'}} />
+                </div>
             </div>
             { this.state.isShowAddLessonPopup ? this.getAddLessonPopup() : null}
         </Layout>;
+    }
+
+    backCourse = (event) => {
+        this.props.router.navigate('/course-detail/' + this.state.course.knowledge_id);
+    }
+
+    saveListLesson = async (event) => {
+        let listLesson = this.state.listLesson;
+        let course = this.state.course;
+        try { 
+            listLesson.forEach((lesson, index) => {
+                lesson.offset = index;
+            })
+            let res = await PatchAPI.getInstance()
+                .setURL(DomainConfig.domainAPI + "/api/courses/listLesson/" + course.knowledge_id)
+                .setToken(Session.getInstance().token)
+                .setBody({
+                    listLesson: listLesson.map((lesson) => ({
+                        lessonid: lesson.knowledge_id,
+                        offset: lesson.offset
+                    }))
+                }).execute();
+            if (res.code != 200) throw new Error(res.message);
+            Toast.getInstance().success("Đã lưu thành công khóa học");
+            this.setState(this.state);
+            this.getListLesson();
+        } catch (e) {
+            Toast.getInstance().error(e.message);
+        }
     }
 
     nullCourse(message){
@@ -120,12 +162,94 @@ class CourseManage extends React.Component{
         let course = this.state.course;
         let listLesson = this.state.listLesson;
         
-        return null;
+        return <div style={{ flexDirection: 'column' }}>
+            {
+                listLesson.map((lesson, index) => {
+                    return <div style={{ margin: '12px 0px'}} key = {index}>
+                        <div style={{ margin: '4px 12px 4px 0px', width: '75px', fontSize: 'larger', fontWeight: 'bold'}}>
+                            {"Bài " + lesson.offset}
+                        </div>
+                        <LessonCard lesson={lesson} style={{width: '100%'}} />
+                        <div style={{width: '150px', margin: '0 0 0 12px'}}>
+                            <div style={{flexDirection: 'column'}}>
+                                <div style={{ margin: '4px 0px'}}>
+                                    <Button text="Up" onclick={() => {this.lessonUp(lesson, index)}} 
+                                        style={{ fontSize: 'smaller', fontWeight: '400', width: '70px'}}
+                                    />
+                                </div>
+                                <div style={{ margin: '4px 0px'}}>
+                                    <Button text="Down" onclick={() => {this.lessonDown(lesson, index)}} 
+                                        style={{ fontSize: 'smaller', fontWeight: '400', width: '70px'}}
+                                    />
+                                </div>
+                            </div>
+                            <div style={{ backgroundColor: 'violet', width: '50px', height: 'auto', margin: '0px 4px'}}>
+                                <Trash style={{fill: 'red', cursor: 'pointer'}} 
+                                onClick={ () => { this.deleteLesson(lesson, index) }} 
+                            />
+                            </div>
+                        </div>
+                    </div>
+                })
+            }
+        </div>;
+    }
+
+    lessonUp = (lesson, index) => {
+        try {
+            let listLesson = this.state.listLesson;
+            if (index == 0) {
+                Toast.getInstance().error("Không thể di chuyển lên");
+                return;
+            }
+            listLesson[index] = listLesson[index - 1];
+            listLesson[index].offset = index;
+            listLesson[index - 1] = lesson;
+            listLesson[index - 1].offset = index - 1;
+            this.setState(this.state);
+        } catch (e) {
+            Toast.getInstance().error(e.message);
+        }   
+    }
+
+    lessonDown = (lesson, index) => {
+        try {
+            let listLesson = this.state.listLesson;
+            if (index >= listLesson.length - 1) {
+                Toast.getInstance().error("Không thể di chuyển xuống");
+                return;
+            }
+            listLesson[index] = listLesson[index + 1];
+            listLesson[index].offset = index;
+            listLesson[index + 1] = lesson;
+            listLesson[index + 1].offset = index + 1;
+            this.setState(this.state);
+        } catch (e) {
+            Toast.getInstance().error(e.message);
+        }
+    }
+
+    deleteLesson = async (lesson, index) => {
+        let course = this.state.course;
+        let listLesson = this.state.listLesson;
+        try {
+            let res = await DeleteAPI.getInstance()
+                .setURL(DomainConfig.domainAPI + "/api/courses/lesson/"
+                    + course.knowledge_id + "/" + lesson.knowledge_id)
+                .setToken(Session.getInstance().token)
+                .execute();
+            if (res.code != 200) throw new Error(res.message);
+            Toast.getInstance().success("Đã xóa khóa học khỏi bài học");
+            this.state.listLesson.splice(index, 1);
+            this.setState(this.state);
+        } catch (e) {
+            Toast.getInstance().error(e.message);
+        }
     }
 
     getAddLessonPopup = () => {
         let { course } = this.state;
-        return <Popup>
+        return <Popup style={{backgroundColor: 'rgba(0, 0, 0, 0.1)'}}>
             <div style={{width: '90%', height: 'auto', maxHeight: '80vh', borderRadius: '6px', flexDirection:'column', padding: '48px 24px'}}>
                 <ChooseLessonPopup course={course}>
 
