84b91f1b0a79eb94a9fa057c7cf744c45ada0ab3
DESKTOP-OET0P0K\Phuc <tranphuc8a@gmail.com>
2023-07-08 23:12:07 +0700

    UD filter

--- app/back-end/src/server.js
+++ app/back-end/src/server.js
@@ -28,8 +28,8 @@ route.route();
 global.connection
 var connectDB = async () => {
     try {
-        global.connection = await mysql.createConnection(dbConfig.localhost);
-        // global.connection = await mysql.createConnection(dbConfig.db4host);
+        // global.connection = await mysql.createConnection(dbConfig.localhost);
+        global.connection = await mysql.createConnection(dbConfig.db4host);
         await global.connection.ping();
         console.log('connect to freaking db successfully');
     } catch (e) {
